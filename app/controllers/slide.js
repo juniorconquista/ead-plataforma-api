72a71bb7e584ecdc5ef95cc0f46a76c5ce472bc4
Júnior <conquista.engcomp@gmail.com>
2019-06-18 19:55:37 -0300

    finish

--- app/controllers/slide.js
+++ app/controllers/slide.js
@@ -9,11 +9,11 @@ export const upload = (req, res) =>
 export const files = (req, res) =>
   services
     .files(req.query.path)
-    .then(response => res.download(req.query.path))
+    .then(() => res.download(req.query.path))
     .catch(err => res.status(400).json({ message: err.message }));
 
 export const list = (_, res) =>
   services
     .list()
-    .then(response => res.json(response[0]))
+    .then(response => res.json(response[response.length - 1]))
     .catch(err => res.status(400).json({ message: err.message }));
